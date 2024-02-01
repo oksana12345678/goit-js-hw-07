3767b49543a32193525ea8849f1f5cd6998ec3fd
oksana12345678 <oksana.tanashuk@gmail.com>
2024-02-01 17:12:36 +0100

    add

--- js/task-2.js
+++ js/task-2.js
@@ -11,30 +11,32 @@ const images = [
     url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
     alt: "Group of Horses Running",
   },
-  {
-    url: "https://images.pexels.com/photos/66898/elephant-cub-tsavo-kenya-66898.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    alt: "Elephant Beside on Baby Elephant",
-  },
-  {
-    url: "https://images.pexels.com/photos/37833/rainbow-lorikeet-parrots-australia-rainbow-37833.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    alt: "Blue Geeen and Orange Parrot",
-  },
-  {
-    url: "https://images.pexels.com/photos/247376/pexels-photo-247376.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    alt: "Zebras on Zebra",
-  },
+  // {
+  //   url: "https://images.pexels.com/photos/66898/elephant-cub-tsavo-kenya-66898.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  //   alt: "Elephant Beside on Baby Elephant",
+  // },
+  // {
+  //   url: "https://images.pexels.com/photos/37833/rainbow-lorikeet-parrots-australia-rainbow-37833.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  //   alt: "Blue Geeen and Orange Parrot",
+  // },
+  // {
+  //   url: "https://images.pexels.com/photos/247376/pexels-photo-247376.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  //   alt: "Zebras on Zebra",
+  // },
 ];
+
 // !!
 const gallery = document.querySelector(".gallery");
 const createGalleryItems = ({ url, alt }) => {
   const listItem = document.createElement("li");
   listItem.classList.add("gallery-item");
   const img = document.createElement("img");
-  images.src = url;
+  img.src = url;
   img.alt = alt;
-  listItem.append(images);
+  listItem.append(img);
 
   return listItem;
 };
 const galleryItems = images.map(createGalleryItems);
 gallery.append(...galleryItems);
+
