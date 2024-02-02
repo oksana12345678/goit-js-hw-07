955b4a21d6fd38ed11f9c9854ff8e970fa1e8377
oksana12345678 <oksana.tanashuk@gmail.com>
2024-02-02 10:04:02 +0100

    add

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,18 @@
+const forms = querySelectorAll(".login-form");
 
+loginForm = forms.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(event.currentTarget);
+  const formValues = Object.fromEntries(formData.entries());
+
+  formData.forEach((value, key) => {
+    formValues[key] = value.trim();
+  });
+  if (formValues.username || formValues.password) {
+    alert("All form fields must be filled in");
+    return;
+  }
+
+  console.log(formValues);
+});
