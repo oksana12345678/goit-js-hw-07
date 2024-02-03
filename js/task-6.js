f3257e19736542e5b5a75af22ba12529bc1a150a
oksana12345678 <oksana.tanashuk@gmail.com>
2024-02-03 15:10:32 +0100

    add-task-6

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,34 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const input = document.querySelector('input[type="number"]');
+const createBtn = document.querySelector("button[data-create]");
+const destroyBtn = document.querySelector("button[data-destroy]");
+const container = document.querySelector("#boxes");
+
+createBtn.addEventListener("click", createBoxes);
+destroyBtn.addEventListener("click", destroyBoxes);
+
+function createBoxes() {
+  const amount = input.valueAsNumber;
+  if (amount >= 0 && amount <= 100) {
+    container.innerHTML = "";
+    const boxes = Array.from({ length: amount }, (_, index) => {
+      const box = document.createElement("div");
+      box.classList.add("box");
+      box.style.backgroundColor = getRandomHexColor();
+      box.style.width = `${30 + index * 10}px`;
+      box.style.height = `${30 + index * 10}px`;
+
+      return box;
+    });
+    container.append(...boxes);
+    input.value = "";
+  } else {
+    alert("Please enter a number between 1 and 100");
+  }
+}
+
+function destroyBoxes() {
+  container.innerHTML = "";
+}
